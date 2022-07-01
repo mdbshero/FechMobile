fa810c878127898b076263720c347a1728d2d3b3
Mdbshero <mdbshero@gmail.com>
2022-07-01 10:25:14 -0500

    Styles practice.

--- App.js
+++ App.js
@@ -1,10 +1,10 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text style={styles.card}>Fech Mobile</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +13,24 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "white",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  layout: {
+    flex: 1,
+    backgroundColor: "#e5e5e5",
+    justifyContent: "center",
+  },
+  card: {
+    width: 100,
+    height: 100,
+    backgroundColor: "white",
+    margin: 16,
+    borderRadius: 2,
+    shadowColor: "black",
+    shadowOpacity: 0.3,
+    shadowRadius: 1,
+    shadowOffset: { height: 1, width: 0.3 },
   },
 });
